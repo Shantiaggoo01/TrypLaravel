78ff5327fcaf075465b08885be1efc875b3aef55
JeanPaul <jean2304paul@gmail.com>
2022-11-26 10:53:18 -0500

    Funciona hasta ahora

--- resources/views/roles/create.blade.php
+++ resources/views/roles/create.blade.php
@@ -14,6 +14,7 @@ Crear Usuarios
 <script>
     $(document).ready(function() {
         $('#example').DataTable({
+
             "language": {
                 "url": "//cdn.datatables.net/plug-ins/1.13.1/i18n/es-ES.json"
             }
